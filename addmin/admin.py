1cb1281874200861512983424b89fa6e8ded080e
surajkrgravitonweb <Suraj.kumar@gravitonweb.com>
2023-09-29 12:04:38 +0530

    admin commit

--- addmin/admin.py
+++ addmin/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import StockFunds, AmountAccount,Stock_form
 # Register your models here.
+admin.site.register(Stock_form)
+admin.site.register(StockFunds)
+admin.site.register(AmountAccount)
